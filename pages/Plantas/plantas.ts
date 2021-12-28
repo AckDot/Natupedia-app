ccbc560916bba541b9ab3538942d50d93bd64947
BOBVO5736 <vargasorellanajoseroberto@gmail.com>
2021-12-27 23:34:16 -0400

    3 files modified, 2 added

--- pages/Plantas/plantas.ts
+++ pages/Plantas/plantas.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import {plants} from './data';
 @Component({
   selector: 'page-plantas',
   templateUrl: 'plantas.html'
 })
 export class PlantasPage{
 
+  plantas: any[] = [];
   constructor(public navCtrl: NavController) {
+    this.initializeItems();
+  }
+  initializeItems(){
+    this.plantas = plants;
+  }
+  
+  getNames(ev) {
+    // Reset items back to all of the items
+    this.initializeItems();
 
+    // set val to the value of the ev target
+    var val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.plantas = plants.filter((item) => {
+        return (item[0].toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
   }
 
 }
