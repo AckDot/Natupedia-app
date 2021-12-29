05e417c5ba1a4369201ab720a8ae54f9dcf34820
BOBVO5736 <vargasorellanajoseroberto@gmail.com>
2021-12-28 23:30:41 -0400

    2 files modified, 1 added, 4 deleted

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { PlantasPage } from '../pages/Plantas/plantas';
 import { DiagnosticoPage } from '../pages/diagnostico/diagnostico';
-
+import {InformacionPage} from '../pages/informacion/informacion'
 
 @NgModule({
   declarations: [
@@ -18,7 +16,8 @@ import { DiagnosticoPage } from '../pages/diagnostico/diagnostico';
     HomePage,
     TabsPage,
     PlantasPage,
-    DiagnosticoPage
+    DiagnosticoPage,
+    InformacionPage
   ],
   imports: [
     BrowserModule,
@@ -30,7 +29,8 @@ import { DiagnosticoPage } from '../pages/diagnostico/diagnostico';
     HomePage,
     TabsPage,
     PlantasPage,
-    DiagnosticoPage
+    DiagnosticoPage,
+    InformacionPage
   ],
   providers: [
     {provide: ErrorHandler, useClass: IonicErrorHandler}
