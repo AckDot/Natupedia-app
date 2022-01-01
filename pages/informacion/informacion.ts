ccfc2ad605bd8379572e68c9f2d3f43be17fb5f8
BOBVO5736 <vargasorellanajoseroberto@gmail.com>
2022-01-01 00:51:16 -0400

    2 files modified , mejora agregando el menu donde apareceran los tags en informacion de plantas

--- pages/informacion/informacion.ts
+++ pages/informacion/informacion.ts
@@ -11,12 +11,5 @@ planta;
   constructor(public navCtrl: NavController , public params :NavParams) {
     this.planta = params.data.item;
   }
- rellenar(){
-  var name = this.planta.name[0];
-  var informacion = name ;
-  informacion += " \n\n Descripcion : \n"+this.planta.descripcion;
-  informacion += "\n\n Productos :\n"+this.planta.productos;
-  informacion += "\n\n Contraindicaciones : \n"+ this.planta.contraindicaciones;
-  return informacion;
- }
+
 }
