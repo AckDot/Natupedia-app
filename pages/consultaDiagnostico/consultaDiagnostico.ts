d3bb1cb8e98a65c023fcac2766792aba141cddcf
BOBVO5736 <vargasorellanajoseroberto@gmail.com>
2022-01-08 22:24:14 -0400

    7 files modified , terminado codigo funcional , inicio de prueba de color a la interfaz

--- pages/consultaDiagnostico/consultaDiagnostico.ts
+++ pages/consultaDiagnostico/consultaDiagnostico.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams ,AlertController } from 'ionic-angular';
 import { plants } from '../Plantas/data';
 import { InformacionPage } from '../informacion/informacion';
 
@@ -9,13 +9,21 @@ import { InformacionPage } from '../informacion/informacion';
 })
 export class consultaDiagnosticoPage {
   plantitas;
-  constructor(public navCtrl: NavController, public params: NavParams) {
+  constructor(public navCtrl: NavController, public params: NavParams, public alertController: AlertController) {
     this.plantitas = params.data.item;
   }
 
-  obtenerPlanta(planta) {
-    let plant = plants.filter( plant => plant.name == planta);
-    console.log(plant);
-    this.navCtrl.push(InformacionPage, { item: plant });
+  obtenerPlanta(plantita) {
+    let p = plants.filter( plant =>  plantita.toLowerCase() == plant.name[0].toLowerCase() );
+    console.log(p);
+    this.navCtrl.push(InformacionPage, { item: p });
+  }
+   mensaje() {
+    const alert =  this.alertController.create({
+      title: 'Consultelo en la seccion Plantas',
+      buttons: ['OK']
+    });
+
+     alert.present();
   }
 }
